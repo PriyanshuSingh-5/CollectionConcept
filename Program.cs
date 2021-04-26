0f76eda3d4c2f02dbcfc63548923bf33e77492de
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-04-26 14:35:29 +0530

    [Priyanshu]modified list

--- Program.cs
+++ Program.cs
@@ -10,8 +10,48 @@ namespace Collections
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to collection");
+            
+        
+        ListDemo();
+        Console.ReadLine();
+        }
+    public static void ListDemo()
+    {
+
+        int[] arr = { 10, 20, 30 };
+
+        //list object creation with integer datatype
+        List<int> primeNumbers = new List<int>();
+
+        //with help of add() adding integer elements into list collection
+        primeNumbers.Add(1); // adding elements using add() method
+        primeNumbers.Add(3);
+        primeNumbers.Add(5);
+        primeNumbers.Add(7);
+
+        //adding array with help of ADDRange method
+        primeNumbers.AddRange(arr);
+
+        //iterate over all elements present in list object
+        foreach (int number in primeNumbers)
+        {
+            Console.Write(number + "\t");
+        }
+        if (primeNumbers.Remove(5))
+        {
+            Console.WriteLine("Removed successfully");
+
+        }
+        else
+        {
+            Console.WriteLine("some error occured");
+        }
+
+        foreach (int number in primeNumbers)
+        {
+            Console.WriteLine(number + "\t");
         }
     }
 }
-           
+}
+     
